22de8397a4b3e30a3aa0507af24bf2743a1b2b2e
vaemjl <vaemjl@163.com>
2017-11-08 10:03:01 +0800

    第三次传

---
+++ simplewind/Core/Library/Vendor/PHPExcel/PHPExcel.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Vendor\PHPExcel;
+require_once 'PHPExcel.php';
+/**
+ * Created by PhpStorm.
+ * User: vst93
+ * Date: 2017/3/14
+ * Time: 11:47
+ */
+class PHPExcel extends \PHPExcel
+{
+}
